bf06750865dccff0d8d5ee70378e774efaaae90b
Jesús Ojeda <48911721+mjesusoj@users.noreply.github.com>
2019-04-23 10:58:55 +0200

    Primary commit and upload files

---
+++ pagina.js
@@ -0,0 +1,11 @@
+// Primer JavaScript, de ejemplo para practicar :)
+
+let nombre = prompt('¿Como te llamas?', '');
+let colorfav = prompt('¿Cuál es tu color favorito?');
+
+if (colorfav == 'Rojo' | colorfav == 'rojo'){
+	alert("Hola" + " " + nombre + ", tu color favorito es el" + " " + colorfav);
+	alert("Mi color favorito también es el rojo");
+}
+else
+	alert("Hola" + " " + nombre + ", tu color favorito es el" + " " + colorfav);
